92dd037186856e63156d0063c4f89e93ffa64b0f
Aaditya <aadityaroblox1@gmail.com>
2026-04-07 00:54:01 +0530

    fixed public folder

---
+++ public/data.js
@@ -0,0 +1,23 @@
+let data = [
+  {
+    name: "Laurent Le Pen",
+    company: "Oxtak",
+    position: "Founder & CEO",
+    priority: "high",
+    score: 5
+  },
+  {
+    name: "Vadym Stepanchuk",
+    company: "Closx",
+    position: "Founder",
+    priority: "high",
+    score: 5
+  },
+  {
+    name: "Pratyush Chowdhary",
+    company: "Orah",
+    position: "Product Owner",
+    priority: "medium",
+    score: 3
+  }
+];
